d7633450015a866f7d2f3cd6995b43f92055aa62
unknown <conjure@CONJURE043957.ens.ad.etsmtl.ca>
2017-01-20 23:44:36 -0500

    Add thrid person character, standard assets

---
+++ Assets/_scripts/powerup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class powerup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<ThirdPersonCharacter>().m_JumpPower = 25.0f;
+            Destroy(this.gameObject);
+        }
+    }
+}
