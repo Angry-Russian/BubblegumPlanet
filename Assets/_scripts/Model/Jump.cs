9f801f8497b1286d3f919253427084edb5bfcd78
am93690 <am93690@ens.etsmtl.ca>
2017-01-21 04:38:33 -0500

    Multiplayer client side : create Jump & Player model for the de-seralisation in json. Connection to the server.

---
+++ Assets/_scripts/Model/Jump.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump {
+
+    private Vector3 position { get; set; }
+    private float time { get; set; }
+}
